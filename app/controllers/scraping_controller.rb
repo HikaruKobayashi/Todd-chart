e2dfc05ff38e97832b3ab928124ca6124100d1a4
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-08-05 19:52:46 +0900

    スクレイピング内容のコメントを書く。

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -1,9 +1,14 @@
 class ScrapingController < ApplicationController
   def index
+    # mechanizeクラスをインスタンス化する。
     agent = Mechanize.new
-    url = 'https://vdata.nikkei.com/newsgraphics/coronavirus-chart-list/'
+    # URLを設定する。
+    url = 'https://web.sapmed.ac.jp/canmol/coronavirus/index.html?n=j'
+    # getメソッドでURLのHTML情報を取得する。
     page = agent.get(url)
-    @elements = page.search('title')
+    # searchメソッドで引数に当てはまる要素を全て取得する。
+    @elements = page.search('index__item__link')
+    # logに出力する。
     puts @elements
   end
 end
