993420b957476448057ecf105a08d0a58071dace
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-08-06 20:42:25 +0900

    ようやく数値をちゃんとスクレイピングできるサイトを発見した。

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -2,12 +2,12 @@ class ScrapingController < ApplicationController
   def index
     # mechanizeクラスをインスタンス化する。
     agent = Mechanize.new
-    # URLを設定する。
-    url = 'https://web.sapmed.ac.jp/canmol/coronavirus/index.html?n=j'
+    # 英国放送協会(BBC)をスクレイピング対象に設定する。
+    url = 'https://www.bbc.com/news/world-51235105'
     # getメソッドでURLのHTML情報を取得する。
     page = agent.get(url)
     # searchメソッドで引数に当てはまる要素を全て取得する。
-    @elements = page.search('index__item__link')
+    @elements = page.search('//*[@id="newsspec-26926"]/div/div/div[2]/div[3]/table/tbody/tr[1]/td[1]')
     # logに出力する。
     puts @elements
   end
